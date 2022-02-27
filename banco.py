0792c0a038cdf949a9ced266c160cbf65f638211
gabrielectronics <99107411+gabrielectronics@users.noreply.github.com>
2022-02-27 18:46:06 -0300

    Update banco.py

--- banco.py
+++ banco.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conexao = sqlite3.connect('nuberUm.db')
 cursor = conexao.cursor()
-cursor.execute('DROP TABLE IF EXISTS testeTabela')
+#cursor.execute('DROP TABLE IF EXISTS testeTabela')
 cursor.execute('CREATE TABLE testeTabela'
             '(Matricula INTEGER PRIMARY KEY , Nome TEXT, Documento INTEGER)')
 
